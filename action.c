d29ff7f850f8cfdf3bfc6069a24098e78ef3aa5c
Paul <paul.cornille-avot@supinfo.com>
2024-02-17 23:08:26 +0100

    addons

---
+++ action.c
@@ -0,0 +1,5 @@
+//
+// Created by corns on 17/02/2024.
+//
+
+#include "action.h"
---
+++ action.h
@@ -0,0 +1,8 @@
+//
+// Created by corns on 17/02/2024.
+//
+
+#ifndef SUPEMON_ACTION_H
+#define SUPEMON_ACTION_H
+
+#endif //SUPEMON_ACTION_H
