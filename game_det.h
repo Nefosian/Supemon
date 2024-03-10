46511711add8197f8ea3a5e06d65dc2c0b83a36b
Paul <paul.cornille-avot@supinfo.com>
2024-03-10 15:10:18 +0100

    suppression fichiers inutilisé

--- game_det.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by Elise on 16/02/2024.
-//
-
-#ifndef SUPEMON_GAME_DET_H
-#define SUPEMON_GAME_DET_H
-
-#endif //SUPEMON_GAME_DET_H
--- save.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by Elise on 16/02/2024.
-//
-
-#ifndef SUPEMON_SAVE_H
-#define SUPEMON_SAVE_H
-
-#endif //SUPEMON_SAVE_H
